48f069ec0ed39984bffd1b724e4c7868e0a8081d
daintycodes <daintycodes@gmail.com>
2025-12-02 21:39:54 +0000

    DB engine: only set check_same_thread for SQLite; avoid invalid DSN option for Postgres

--- app/database.py
+++ app/database.py
@@ -2,7 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from .config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# Only pass `check_same_thread` when using SQLite; PostgreSQL/others don't accept it.
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args) if connect_args else create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
